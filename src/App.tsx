c729154cdc6160afd3693cc350467f6e280effd8
Jorge Saa <piojo.sistematico@gmail.com>
2023-07-28 20:24:02 -0400

    basic html

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import slider from "./assets/images/icon-slider.svg";
+import check from "./assets/images/icon-check.svg";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main>
+      <section className="title">
+        <h1> Simple, traffic-based pricing</h1>
+        <p>Sign-up for our 30-day trial. No credit card required. </p>
+      </section>
+      <section className="plan-section">
+        <span>XXX Pageviews</span>
+        <div className="bar">
+          <img src={slider} alt="" />
+        </div>
+        <h2>$ XXX /month</h2>
+        <div>
+          <div>
+            <span>Monthly Billing</span>
+            <span>SLIDER</span>
+            <span>Yearly Billing</span>
+            <span>25% discount</span>
+          </div>
+          <div>
+            <ul>
+              <li>
+                <img src={check} alt="" />
+                <span>Unlimited websites</span>
+              </li>
+              <li>
+                <img src={check} alt="" />
+                <span>100% data ownership</span>
+              </li>
+              <li>
+                <img src={check} alt="" />
+                <span>Email reports</span>
+              </li>
+            </ul>
+            <button>Start my trial</button>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
